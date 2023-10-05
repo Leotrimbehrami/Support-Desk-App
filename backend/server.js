8d35445f901a525f34fd9ca57a7217edbb424853
dci-student <leotrim.behrami@uni-pr.edu>
2023-10-05 16:13:33 +0200

    server started

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,11 @@
-console.log("Server..")
+import express from 'express'
+import "dotenv/config";
+
+const PORT = process.env.PORT || 4500
+const app = express()
+
+app.get('/', (req,res) => {
+  res.status(201).json({message: 'Welcome'})
+})
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
