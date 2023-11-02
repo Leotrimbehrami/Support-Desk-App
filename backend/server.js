c9e0b175243d3914c308c5b8c3e5a9cf1e60cf18
dci-student <leotrim.behrami@uni-pr.edu>
2023-11-02 20:24:07 +0100

    ready for deploy

--- backend/server.js
+++ backend/server.js
@@ -1,28 +1,40 @@
-import express from 'express'
-import colors from 'colors'
+import path from "path";
+import express from "express";
+import colors from "colors";
 import "dotenv/config";
-import { errorHandler } from './middleware/errorMiddleware.js';
-import { userRouter } from './routes/userRoutes.js';
-import { connectDB } from './config/db.js'
-import { ticketRouter } from './routes/ticketRoutes.js';
-const PORT = process.env.PORT || 4500
+import { errorHandler } from "./middleware/errorMiddleware.js";
+import { userRouter } from "./routes/userRoutes.js";
+import { connectDB } from "./config/db.js";
+import { ticketRouter } from "./routes/ticketRoutes.js";
+const PORT = process.env.PORT || 4500;
 
 // connect to database
-connectDB()
+connectDB();
 
-const app = express()
+const app = express();
 
-app.use(express.json())
-app.use(express.urlencoded({extended: false}))
-
-app.get('/', (req,res) => {
-  res.status(201).json({message: 'Welcome'})
-})
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 // Routes
-app.use('/api/users', userRouter)
+app.use("/api/users", userRouter);
 app.use("/api/tickets", ticketRouter);
 
-app.use(errorHandler)
+// Serve Frontend
+if (process.env.NODE_ENV === "production") {
+  // Set build folder as static
+  app.use(express.static(path.join(__dirname, "../frontend/build")));
 
-app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
+  // FIX: below code fixes app crashing on refresh deployment
+  app.get("*", (_, res) => {
+    res.sendFile(path.join(__dirname, "../frontend/build/index.html"));
+  });
+} else {
+  app.get("/", (_, res) => {
+    res.status(200).json({ message: "Welcome to the Support Desk API" });
+  });
+}
+
+app.use(errorHandler);
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
