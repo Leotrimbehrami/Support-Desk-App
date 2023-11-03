79ab200ea18c458ee30590c1d92f6460b97dcdfb
dci-student <leotrim.behrami@uni-pr.edu>
2023-11-03 23:02:48 +0100

    ok

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,11 +1,24 @@
+// import mongoose from "mongoose";
+
+//  export const connectDB = async () => {
+//  try {
+//   const connect = await mongoose.connect(process.env.MONGO_URI)
+//   console.log(`MongoDB Connected: ${connect.connection.host}`.cyan.underline)
+//  } catch (error) {
+//   console.log(`Error: ${error.message}`.red.underline.bold)
+//   process.exit(1)
+//  }
+// }
+
 import mongoose from "mongoose";
 
- export const connectDB = async () => {
- try {
-  const connect = await mongoose.connect(process.env.MONGO_URI)
-  console.log(`MongoDB Connected: ${connect.connection.host}`.cyan.underline)
- } catch (error) {
-  console.log(`Error: ${error.message}`.red.underline.bold)
-  process.exit(1)
- }
-}
+export const connectDB = async () => {
+  try {
+    console.log("MONGO_URI is: ", process.env.MONGO_URI); // Hinzugefügte Zeile
+    const connect = await mongoose.connect(process.env.MONGO_URI);
+    console.log(`MongoDB Connected: ${connect.connection.host}`.cyan.underline);
+  } catch (error) {
+    console.log(`Error: ${error.message}`.red.underline.bold);
+    process.exit(1);
+  }
+};
